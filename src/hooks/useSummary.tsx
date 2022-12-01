a751546d14423426cc191f87f755d7d8d7f06c3c
jeffersontk <jefferson.britotk@gmail.com>
2022-12-01 11:43:47 -0300

    add useMemo

--- src/hooks/useSummary.tsx
+++ src/hooks/useSummary.tsx
@@ -1,25 +1,27 @@
-import { useContext } from 'react'
+import { useContext, useMemo } from 'react'
 import { TransactionContext } from '../contexts/TransactionsContext'
 
 export function useSummary() {
   const { transactionsBySummary } = useContext(TransactionContext)
 
-  const summary = transactionsBySummary.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+  const summary = useMemo(() => {
+    return transactionsBySummary.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactionsBySummary])
   return summary
 }
